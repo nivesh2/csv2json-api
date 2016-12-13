f3cff0ad06428fe1f993f24b27d222e6d20523fa
Nivesh Singh <nivesh2@gmail.com>
2016-12-13 09:32:12 +0530

    streaming csv2json convertion up and running

---
+++ app/csv2json/convert.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/*
+ * Streams are used for fast processing.
+ * Fetches CSV content from remote url (query string)
+ * and converts it into json
+ * and streams the json response back to the client
+ */
+
+var request = require('request'),
+    csv2json = require('csv2json'),
+    debug = require('debug')('instarem:convert');
+
+function convert(uri, res, callback){
+
+  request.head(uri, function(err, response, body){
+
+      if(err){
+          debug('Error',err);
+          callback(err);
+      }
+
+    debug('content-type:', response.headers['content-type']);
+    debug('content-length:', response.headers['content-length']);
+
+    //setting response content-type to JSON    
+    res.set('Content-Type', 'application/json');
+
+    request(uri).pipe(csv2json({
+            separator: ','
+        })).pipe(res).on('close', callback);
+    });
+}
+
+module.exports = exports = function(req,res,next){
+    debug('Query URL:',req.query);
+    
+    convert(req.query.q,res, function(err){
+        if(err){
+            next(err);
+        }
+        debug('done');
+    });
+} 
+
--- app/csv2json/index.js
+++ app/csv2json/index.js
@@ -1,11 +1,11 @@
 'use strict';
 
+/* 
+ * routing module for this feature
+ */
 var express = require('express');
 var router = express.Router();
 
-
-router.get('/csv/to/json', function (req, res,next) {
-  res.send('Welcome');
-});
+router.get('/csv/to/json',require('./convert'));
 
 module.exports = exports = router;
